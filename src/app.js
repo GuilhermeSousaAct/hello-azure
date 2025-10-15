32eba8db6dc911003cde5b65c9a998da517ef3fe
Guilherme Sousa <guilhermesousa.costa@actdigital.com>
2025-10-15 08:03:10 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+	const { name } = req.query;
+
+	if (!name) return res.status(200).json({ message: `Hello ${name}` });
+
+	return res.status(200).json({ message: 'Hello World!' });
+});
